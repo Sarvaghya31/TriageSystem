a6a42e4f0f53a8542388ced7128a3a721d63dd6f
Sarvaghya31 <sarvaghyajoshi2003@gmail.com>
2025-09-07 09:22:02 +0530

    mailtrap removed

--- ai-ticket-assistant/utils/mailer.js
+++ ai-ticket-assistant/utils/mailer.js
@@ -1,18 +1,27 @@
 import nodemailer from "nodemailer"
 export const sendMail = async(to,subject,text)=>{
     try{
+  //      const transporter = nodemailer.createTransport({
+  // host: process.env.MAILTRAP_SMTP_HOST,
+  // port: process.env.MAILTRAP_SMTP_PORT,
+  // secure: false, 
+  // auth: {
+  //   user: process.env.MAILTRAP_SMTP_USER,
+  //   pass: process.env.MAILTRAP_SMTP_PASS,
+  // },
+
        const transporter = nodemailer.createTransport({
-  host: process.env.MAILTRAP_SMTP_HOST,
-  port: process.env.MAILTRAP_SMTP_PORT,
-  secure: false, 
+  host: process.env.SMTP_HOST,
+  port: Number(process.env.SMTP_PORT),
+  secure:false,
   auth: {
-    user: process.env.MAILTRAP_SMTP_USER,
-    pass: process.env.MAILTRAP_SMTP_PASS,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
   },
 });
 
    const info = await transporter.sendMail({
-    from: 'Inngest TMS',
+    from: 'Inngest TMS <sarvaghyajoshi2003@gmail.com>',
     to,
     subject,
     text, 
